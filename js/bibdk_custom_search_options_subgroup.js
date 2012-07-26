9a497b9e44989878724eba0e751e883769cdd0ee
Jorgen G. Nielsen <jgn@dbc.dk>
2012-07-26 13:36:10 +0200

    fixed another javascript bug on duplicate subvalues

--- js/bibdk_custom_search_options_subgroup.js
+++ js/bibdk_custom_search_options_subgroup.js
@@ -6,11 +6,11 @@
   });
 
   Drupal.bibdkCustomSearchOptionsSubgroup = function() {
-    $.each(Drupal.settings.expanded, function(eid, elementGroup) {
+    $.each(Drupal.settings.expanded, function(element, elementGroup) {
       $.each(elementGroup, function(key, val) {
 
         // create toggle element and bind an event handler to it
-        var ExpandTrigger = $('<div data="subgroup-' + val.parentElemId + '" class="toggle-subgroup"> + </div>');
+        var ExpandTrigger = $('<div data="subgroup-' + val.elementId + '-' + val.parentValueId + '" class="toggle-subgroup"> + </div>');
         ExpandTrigger.toggle(function() {
           var subgroup = $(this).attr('data');
           $('#' + subgroup).show('fast');
@@ -18,33 +18,33 @@
           var subgroup = $(this).attr('data');
           $('#' + subgroup).hide('fast');
         });
-        $('#edit-bibdk-custom-search-element-' + eid + ' input[value=' + val.parentElem + ']').after(ExpandTrigger);
+        $('#edit-bibdk-custom-search-element-' + val.elementId + ' input[value=' + val.parentValue + ']').after(ExpandTrigger);
 
         // uncheck child option if a parent option is selected.
-        $('#edit-bibdk-custom-search-element-' + eid + ' input[value=' + val.parentElem + ']').change(function() {
+        $('#edit-bibdk-custom-search-element-' + val.elementId + ' input[value=' + val.parentValue + ']').change(function() {
           if ( $(this).is(':checked') ) {
             $.each(val.childElem, function(key2, val2) {
-              $('#edit-bibdk-custom-search-element-' + eid + ' input[value=' + val2 + ']').attr('checked', false);
+              $('#edit-bibdk-custom-search-element-' + val.elementId + ' input[value=' + val2 + ']').attr('checked', false);
             });
           }
         });
 
         // uncheck parent option if a child option is selected.
         $.each(val.childElem, function(key2, val2) {
-          $('#edit-bibdk-custom-search-element-' + eid + ' input[value=' + val2 + ']').change(function() {
+          $('#edit-bibdk-custom-search-element-' + val.elementId + ' input[value=' + val2 + ']').change(function() {
             if ( $(this).is(':checked') ) {
-              $('#edit-bibdk-custom-search-element-' + eid + ' input[value=' + val.parentElem + ']').attr('checked', false);
+              $('#edit-bibdk-custom-search-element-' + val.elementId + ' input[value=' + val.parentValue + ']').attr('checked', false);
             }
           });
         });
 
         // move child options into a div under parent.
-        var subElem = $('<div id="subgroup-' + val.parentElemId + '" class="bibdk-subgroup"></div>');
+        var subElem = $('<div id="subgroup-' + val.elementId + '-' + val.parentValueId + '" class="bibdk-subgroup"></div>');
         $.each(val.childElem, function(key2, val2) {
-          subElem.append( $('#edit-bibdk-custom-search-element-' + eid + ' input[value=' + val2 + ']').closest("div") );
+          subElem.append( $('#edit-bibdk-custom-search-element-' + val.elementId + ' input[value=' + val2 + ']').closest("div") );
         });
 
-        $('#edit-bibdk-custom-search-element-' + eid + ' input[value=' + val.parentElem + ']').closest("div").after(subElem);
+        $('#edit-bibdk-custom-search-element-' + val.elementId + ' input[value=' + val.parentValue + ']').closest("div").after(subElem);
       });
     });
   };
