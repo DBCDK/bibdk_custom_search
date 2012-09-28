ef8a7e881a51a28cc27aceb54bd36f8b3217aa1f
Søren Vibjerg <vibjerg@gmail.com>
2012-09-28 13:01:56 +0200

    Added theme function for advanced search

--- theme/bibdk_custom_search-search-element-form.tpl.php
+++ theme/bibdk_custom_search-search-element-form.tpl.php
@@ -10,7 +10,6 @@
 <?php if ( $form['#help_text'] ) { print '<p class="helptext" title="' . $form['#help_text'] . '">?</p>'; } ?>
 
 <div class="bibdk-custom-search-element">
-
 <?php print drupal_render_children($form); ?>
 
 </div>
