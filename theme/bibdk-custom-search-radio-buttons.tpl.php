f5f7574d7959b767f3a9da4ae5c600ce931ac275
JrgenGNielsen <jgn@dbc.dk>
2014-03-17 16:11:26 +0100

    support for multiple subgroups

--- theme/bibdk-custom-search-radio-buttons.tpl.php
+++ theme/bibdk-custom-search-radio-buttons.tpl.php
@@ -2,10 +2,9 @@
 
 /*
 See: function template_preprocess_bibdk_custom_search_radio_buttons() in template.php
-$first = $form['#data_toggle_value'][0];
-$last  = end($form['#data_toggle_value']);
-$form[$first]['#prefix'] = '<fieldset id="edit-term-' . $form['#data_toggle_key'] . '" class="sub-elements form-wrapper" data-child="' . $form['#data_toggle_key'] . '" style="display: block;">';
-$form[$last]['#suffix'] = '</fieldset>';
+  $variables['form'][$key]['#title'] = '<span data-child="' . drupal_html_id($key) . '" class="toggle-subgroup"> + </span>' . $key;
+  $variables['form'][$first]['#prefix'] = '<fieldset id="edit-term-' . $key . '" class="sub-elements form-wrapper" data-child="' . $key . '" style="display: block;">';
+  $variables['form'][$last]['#suffix'] = '</fieldset>';
 */
 
 echo drupal_render_children($form);
