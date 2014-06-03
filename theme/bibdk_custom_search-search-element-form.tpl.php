1ba7c01ea9dfe5e87d20a0fcb4e0d2c2f5d101e6
JrgenGNielsen <jgn@dbc.dk>
2014-06-03 12:37:36 +0200

    add label element to input header, and suppress default label

--- theme/bibdk_custom_search-search-element-form.tpl.php
+++ theme/bibdk_custom_search-search-element-form.tpl.php
@@ -4,7 +4,7 @@
  * Theme implementation for bib.dk custom search elements.
  */
 ?>
-<div class='fieldset-legend'><?php print $title; ?></div>
+<div class='fieldset-legend'><label for="<?php print $element_id; ?>"><?php print $title; ?></label></div>
 <div class="fieldset-description"><?php print $description;?></div>
 <p class="helptext" title="<?php print $help; ?>">?</p>
 <div class="bibdk-custom-search-element clearfix">
