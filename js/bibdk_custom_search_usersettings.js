f4db64c311444e2e4bc4847f1e9b7921ba4bcda7
Søren Vibjerg <vibjerg@gmail.com>
2014-06-24 11:34:25 +0200

    reverse when subgroups are hidden

--- js/bibdk_custom_search_usersettings.js
+++ js/bibdk_custom_search_usersettings.js
@@ -7,7 +7,7 @@
                 console.log(element);
                 var parentId = this.getAttribute('data-setting-child');
                 var parentValue = $('[data-setting-parent=' + parentId + ']:checked').val();
-                if (parentValue == 0) {
+                if (parentValue == 1) {
                     $('[data-setting-child=' + parentId + ']').hide();
                 }
             });
@@ -15,7 +15,7 @@
             $('[data-setting-parent]', context).change(function (e) {
                 var parentId = this.getAttribute('data-setting-parent')
 
-                    if (this.value == 0) {
+                    if (this.value == 1) {
                         $('[data-setting-child=' + parentId + '] input[value=0]').attr('checked', 'checked');
                     }
 
