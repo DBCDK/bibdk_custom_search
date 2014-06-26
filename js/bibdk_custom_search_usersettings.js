ea50ef07bc2f9aba04f1e1a85cfa09e903611d7b
Søren Vibjerg <vibjerg@gmail.com>
2014-06-26 11:03:35 +0200

    indent

--- js/bibdk_custom_search_usersettings.js
+++ js/bibdk_custom_search_usersettings.js
@@ -3,7 +3,7 @@
 
         attach: function (context) {
 
-            $('[data-setting-child]', context).each(function (i, element){
+            $('[data-setting-child]', context).each(function (i, element) {
                 console.log(element);
                 var parentId = this.getAttribute('data-setting-child');
                 var parentValue = $('[data-setting-parent=' + parentId + ']:checked').val();
@@ -15,11 +15,11 @@
             $('[data-setting-parent]', context).change(function (e) {
                 var parentId = this.getAttribute('data-setting-parent')
 
-                    if (this.value == 1) {
-                        $('[data-setting-child=' + parentId + '] input[value=0]').attr('checked', 'checked');
-                    }
+                if (this.value == 1) {
+                    $('[data-setting-child=' + parentId + '] input[value=0]').attr('checked', 'checked');
+                }
 
-                 $('[data-setting-child=' + parentId + ']').toggle();
+                $('[data-setting-child=' + parentId + ']').toggle();
             });
         },
         detach: function (context) {
